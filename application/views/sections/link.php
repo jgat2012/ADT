beaf5f70c4eb1ac555e2a43e538997c7b685202d
jgat2012 <gauthierabdala@gmail.com>
2014-12-08 23:53:42 +0300

    Merging

--- application/views/sections/link.php
+++ application/views/sections/link.php
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 <link type="text/css" rel="stylesheet" href="http://localhost:80/ADT/assets/cache/14160079181cd61b25796ccb6cbf22d8e1527cc816.css" media="screen" />
 <script type="text/javascript" src="http://localhost:80/ADT/assets/cache/1416007914ed02267cd3d575ee044c19024723e9cf.js" charset="UTF-8"></script>
-=======
-<link type="text/css" rel="stylesheet" href="http://localhost:80/ADT/assets/cache/14160598981cd61b25796ccb6cbf22d8e1527cc816.css" media="screen" />
-<script type="text/javascript" src="http://localhost:80/ADT/assets/cache/1416838227ed02267cd3d575ee044c19024723e9cf.js" charset="UTF-8"></script>
->>>>>>> b8c905d7492bf807cbacf480656c60de2e8879c4
